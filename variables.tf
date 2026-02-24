6c4b5ab7d1817e4bdcc3c3c10c84c1ec0dd1c438
Mark Lindros <26391158+mlindros@users.noreply.github.com>
2026-02-23 22:37:46 -0600

    Initial OCI Terraform project structure

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  description = "OCI region"
+  type        = string
+}
+
+variable "tenancy_ocid" {
+  description = "OCI tenancy OCID"
+  type        = string
+}
+
+variable "user_ocid" {
+  description = "OCI user OCID"
+  type        = string
+}
+
+variable "fingerprint" {
+  description = "API key fingerprint"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to OCI API private key"
+  type        = string
+}
